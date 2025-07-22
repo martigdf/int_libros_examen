3a47978f7058bb5afd60aa52d13a80f1d07ff55f
martigdf <martina.guzman@correo.ucu.edu.uy>
2025-07-21 21:58:39 -0300

    otro import que estaba mal ahora si ya sin errores

--- Backend/src/routes/auth/login/login.ts
+++ Backend/src/routes/auth/login/login.ts
@@ -1,4 +1,4 @@
-import { FastifyPluginAsyncTypebox } from '../@fastify/type-provider-typebox'
+import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { query } from '../../../services/database.js';
 
 const authRoute: FastifyPluginAsyncTypebox = async (fastify, opts): Promise<void> => {
