c7da2300d76b64eab33d93a9b84c4117de2ab665
martigdf <martina.guzman@correo.ucu.edu.uy>
2025-07-23 23:50:07 -0300

    ahora si

--- frontend/src/app/pages/menu-loans/menu-loans.page.ts
+++ frontend/src/app/pages/menu-loans/menu-loans.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonMenuButton, IonMenuToggle, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonMenuButton, IonMenuToggle, IonTitle, IonToolbar, IonButton, IonItem } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-menu-loans',
   templateUrl: './menu-loans.page.html',
   styleUrls: ['./menu-loans.page.scss'],
   standalone: true,
-  imports: [IonButton, IonMenuButton, IonMenuToggle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonItem, IonButton, IonMenuButton, IonMenuToggle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
 })
 export class MenuLoansPage implements OnInit {
 
